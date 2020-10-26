6e9ee16b5783fa5dbf8c5c35bb4eda78efc8a09b
Rajaa Omary <omary.rajaa@gmail.com>
2020-10-26 23:17:54 +0000

    Create function assert

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(String.fromCodePoint(0x2714) + ' Assertion Passed: ' + actual + ' === ' + expected);
+  } else {
+    console.log(String.fromCodePoint(0x274C) + ' Assertion Failed: ' + actual + ' !== ' + expected);
+  }
+
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(10, 130);
+assertEqual("rajaa", "rajaa");
