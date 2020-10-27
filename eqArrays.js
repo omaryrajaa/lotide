a5ad646b45869b1415d3efbb84579440dffbeb48
Rajaa Omary <omary.rajaa@gmail.com>
2020-10-27 22:01:47 +0000

    Implement eqArrays Lotide

---
+++ eqArrays.js
@@ -0,0 +1,36 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`${String.fromCodePoint(0x2714)} Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`${String.fromCodePoint(0x274C)} Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+const eqArrays = function(array1, array2) {
+  let equal = '';
+  if (array1.length === array2.length) {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] === array2[i]) {
+        equal = true;
+      } else {
+        equal = false;
+      }
+    }
+  } else {
+    equal = false;
+  }
+  return equal;
+};
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2]), true);
+assertEqual(eqArrays([], []), true);
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+
