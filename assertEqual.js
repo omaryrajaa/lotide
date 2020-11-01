d3881abc56776ed840f3f27d745ffc28cfecd1dd
Rajaa Omary <omary.rajaa@gmail.com>
2020-11-01 03:18:04 +0000

    Refactor

--- assertEqual.js
+++ assertEqual.js
@@ -8,8 +8,4 @@ const assertEqual = function(actual, expected) {
 
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(10, 130);
-assertEqual("rajaa", "rajaa");
+module.exports = assertEqual;
---
+++ test/assertEqualTest.js
@@ -0,0 +1,7 @@
+const assertEqual = require('../assertEqual');
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(10, 130);
+assertEqual("rajaa", "rajaa");
